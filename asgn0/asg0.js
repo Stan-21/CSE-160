5961f2462db37bd9957c4434e1ac00aaeb6802aa
Stan-21 <shung951@gmail.com>
2025-04-03 20:37:59 -0700

    Finished up to step 5

--- asgn0/asg0.js
+++ asgn0/asg0.js
@@ -40,3 +40,30 @@ function handleDrawEvent() {
   var v2 = new Vector3([document.getElementById('x2').value, document.getElementById('y2').value]);
   drawVector(v2, "blue");
 }
+
+function handleDrawOperationEvent() {
+  const canvas = document.getElementById('example');
+  const ctx = canvas.getContext("2d");
+
+  ctx.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
+  ctx.fillRect(0, 0, canvas.clientWidth, canvas.clientHeight);
+
+  var v1 = new Vector3([document.getElementById('x1').value, document.getElementById('y1').value]);
+  drawVector(v1, "red");
+  var v2 = new Vector3([document.getElementById('x2').value, document.getElementById('y2').value]);
+  drawVector(v2, "blue");
+  if (document.getElementById('operation').value == "add") {
+    drawVector(v1.add(v2), "green");
+  } 
+  if (document.getElementById('operation').value == "subtract") {
+    drawVector(v1.sub(v2), "green");
+  } 
+  if (document.getElementById('operation').value == "multiply") {
+    drawVector(v1.mul(document.getElementById('scalar').value), "green");
+    drawVector(v2.mul(document.getElementById('scalar').value), "green");
+  } 
+  if (document.getElementById('operation').value == "divide") {
+    drawVector(v1.div(document.getElementById('scalar').value), "green");
+    drawVector(v2.div(document.getElementById('scalar').value), "green");
+  } 
+}
