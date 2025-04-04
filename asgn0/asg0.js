9614bdb0a754f0f6cb6977ea75394bc67a0c5432
Stan-21 <shung951@gmail.com>
2025-04-03 20:15:43 -0700

    Finished up to step 4

--- asgn0/asg0.js
+++ asgn0/asg0.js
@@ -11,7 +11,6 @@ function main() {
   var ctx = canvas.getContext('2d');
   var v1 = new Vector3([2.25, 2.25, 0]);
 
-  console.log("asdf");
   // Draw a blue rectangle
   //ctx.fillStyle = 'rgba(0, 0, 255, 1.0)'; // Set color to blue
   ctx.fillRect(0, 0, canvas.clientWidth, canvas.clientHeight);        // Fill a rectangle with the color
@@ -25,7 +24,19 @@ function drawVector(v, color) {
   ctx.beginPath();
   ctx.strokeStyle = color;
   ctx.moveTo(canvas.clientWidth / 2, canvas.clientHeight / 2);
-  console.log(v.elements[0])
   ctx.lineTo((canvas.clientWidth / 2) + (v.elements[0] * 20), (canvas.clientHeight / 2) - (v.elements[1] * 20)); // Change numbers to get canvas size???
   ctx.stroke();
+}
+
+function handleDrawEvent() {
+  const canvas = document.getElementById('example');
+  const ctx = canvas.getContext("2d");
+
+  ctx.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
+  ctx.fillRect(0, 0, canvas.clientWidth, canvas.clientHeight);
+
+  var v1 = new Vector3([document.getElementById('x1').value, document.getElementById('y1').value]);
+  drawVector(v1, "red");
+  var v2 = new Vector3([document.getElementById('x2').value, document.getElementById('y2').value]);
+  drawVector(v2, "blue");
 }
