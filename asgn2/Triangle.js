04568a1be2f950e429c4b236798949e63b5abc67
Stan-21 <shung951@gmail.com>
2025-04-24 01:10:55 -0700

    Progress

---
+++ asgn2/Triangle.js
@@ -0,0 +1,106 @@
+class Triangle {
+    constructor() {
+        this.type = 'triangle';
+        this.position = [0.0, 0.0, 0.0];
+        this.color = [1.0, 1.0, 1.0, 1.0];
+        this.size = 5.0;
+        this.points = [];
+        this.state = 0;
+    }
+
+    render() {
+        var xy = this.position;
+        var rgba = this.color;
+        var size = this.size;
+    
+        // Pass the position of a point to a_Position variable
+        gl.vertexAttrib3f(a_Position, xy[0], xy[1], 0.0);
+        // Pass the color of a point to u_FragColor variable
+        gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
+        // Pass the size of a point to u_Size variable
+        gl.uniform1f(u_Size, size)
+        // Draw
+        if (this.points.length == 0) {
+            var d = this.size / 200.0; // maybe could move stuff above to if statements?
+            drawTriangle([xy[0], xy[1], xy[0]+d, xy[1], xy[0], xy[1]+d]);
+        } else {
+            drawTriangle([this.points[0] / this.size, this.points[1] / this.size - 0.1, this.points[2] / this.size,
+                this.points[3] / this.size - 0.1, this.points[4] / this.size, this.points[5] / this.size - 0.1]
+            );
+        }
+    }
+
+    cycleColors() {
+        var interval = (1.0 / 255) * 20;
+        if (this.state == 0) {
+          this.color[1] += interval;
+          if (this.color[1] >= 1.0) {
+            this.color[1] = 1.0;
+            this.state = 1;
+          }
+        }
+        if (this.state == 1) {
+          this.color[0] -= interval;
+          if (this.color[0] <= 0) {
+            this.color[0] = 0;
+            this.state = 2;
+          }
+        }
+        if (this.state == 2) {
+          this.color[2] += interval;
+          if (this.color[2] >= 1.0) {
+            this.color[2] = 1.0;
+            this.state = 3;
+          }
+        }
+        if (this.state == 3) {
+          this.color[1] -= interval;
+          if (this.color[1] <= 0) {
+            this.color[1] = 0.0;
+            this.state = 4;
+          }
+        }
+        if (this.state == 4) {
+          this.color[0] += interval;
+          if (this.color[0] >= 1.0) {
+            this.color[0] = 1.0;
+            this.state = 5;
+          }
+        }
+        if (this.state == 5) {
+          this.color[2] -= interval;
+          if (this.color[2] <= 0) {
+            this.color[2] = 0.0;
+            this.state = 0;
+          }
+        }
+      }
+}
+
+function drawTriangle(vertices) {
+    // var vertices = new Float32Array([
+    //    0, 0.5, -0.5, -0.5, 0.5, -0.5
+    //]);
+
+    var n = 3;
+
+    var vertexBuffer = gl.createBuffer();
+    if (!vertexBuffer) {
+        console.log("Failed to create the buffer object");
+        return -1;
+    }
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.DYNAMIC_DRAW);
+
+    //var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+    //if (a_Position < 0) {
+    //   console.log('Failed to get the storage location of a_Position');
+    //    return -1;
+    //}
+
+    gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(a_Position);
+    gl.drawArrays(gl.TRIANGLES, 0, n);
+}
---
+++ asgn2/asg2.js
@@ -0,0 +1,407 @@
+// ColoredPoint.js (c) 2012 matsuda
+// Vertex shader program
+var VSHADER_SOURCE = `
+  attribute vec4 a_Position;
+  uniform mat4 u_ModelMatrix;
+  uniform mat4 u_GlobalRotateMatrix;
+  void main() {
+    gl_Position = u_GlobalRotateMatrix * u_ModelMatrix * a_Position;
+  }`
+
+// Fragment shader program
+var FSHADER_SOURCE = `
+  precision mediump float;
+  uniform vec4 u_FragColor;
+  void main() {
+    gl_FragColor = u_FragColor;
+  }` 
+
+// Global variables
+let canvas;
+let gl;
+let a_Position;
+let u_FragColor;
+let u_Size;
+let u_ModelMatrix;
+let u_GlobalRotateMatrix;
+
+// Constants
+const POINT = 0;
+const TRIANGLE = 1;
+const CIRCLE = 2;
+
+let g_selectedColor=[1.0, 1.0, 1.0, 1.0];
+let g_selectedSize=10.0;
+let g_selectedType=POINT;
+let g_selectedSegment=10;
+let g_globalAngle=0;
+
+function setupWebGL() {  
+  // Retrieve <canvas> element
+  canvas = document.getElementById('webgl');
+
+  // Get the rendering context for WebGL
+  //gl = getWebGLContext(canvas);
+  gl = canvas.getContext("webgl", {perserveDrawingBuffer: true});
+  if (!gl) {
+    console.log('Failed to get the rendering context for WebGL');
+    return;
+  }
+  gl.enable(gl.DEPTH_TEST);
+}
+
+function connectVariablesToGLSL() {
+  // Initialize shaders
+  if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+    console.log('Failed to intialize shaders.');
+    return;
+  }
+
+  // // Get the storage location of a_Position
+  a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+  if (a_Position < 0) {
+    console.log('Failed to get the storage location of a_Position');
+    return;
+  }
+
+  // Get the storage location of u_FragColor
+  u_FragColor = gl.getUniformLocation(gl.program, 'u_FragColor');
+  if (!u_FragColor) {
+    console.log('Failed to get the storage location of u_FragColor');
+    return;
+  }
+
+  u_ModelMatrix = gl.getUniformLocation(gl.program, 'u_ModelMatrix');
+  if (!u_ModelMatrix) {
+    console.log('Failed to get the storage location of u_ModelMatrix');
+    return;
+  }
+
+  u_GlobalRotateMatrix = gl.getUniformLocation(gl.program, 'u_GlobalRotateMatrix');
+  if (!u_GlobalRotateMatrix) {
+    console.log('Failed to get the storage location of u_GlobalRotateMatrix');
+    return;
+  }
+
+  var identityM = new Matrix4();
+  gl.uniformMatrix4fv(u_ModelMatrix, false, identityM.elements);
+
+
+}
+
+var intervalId;
+var cycle = false;
+
+function addActionsForHTMLUI() {
+  // Slider Events
+  document.getElementById('redSlide').addEventListener('mouseup', function() { g_selectedColor[0] = this.value / 100;});
+  document.getElementById('greenSlide').addEventListener('mouseup', function() { g_selectedColor[1] = this.value / 100;});
+  document.getElementById('blueSlide').addEventListener('mouseup', function() { g_selectedColor[2] = this.value / 100;});
+
+  document.getElementById('shapeSlide').addEventListener('mouseup', function() { g_selectedSize = this.value;});
+  document.getElementById('circleSlide').addEventListener('mouseup', function() { g_selectedSegment = this.value;});
+
+  document.getElementById('angleSlide').addEventListener('mousemove', function() { g_globalAngle = this.value; renderAllShapes(); });
+}
+
+var undone_Shapes = [];
+
+function rainbow() {
+  var startTime = performance.now();
+
+
+  // Clear <canvas>
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  var len = g_shapesList.length;
+  for(var i = 0; i < len; i++) {
+    g_shapesList[i].cycleColors();
+    g_shapesList[i].render();
+  }
+
+  var duration = performance.now() - startTime;
+}
+
+function undo() {
+  if (g_shapesList.length > 0) {
+    undone_Shapes.push(g_shapesList.pop());
+  }
+
+  document.getElementById('redSlide').value = 0;
+  renderAllShapes();
+  
+}
+
+function redo() {
+  if (undone_Shapes.length > 0) {
+    g_shapesList.push(undone_Shapes.pop());
+  }
+  renderAllShapes();
+}
+
+function oshawott() {
+  g_shapesList = []; // Clearing canvas
+  renderAllShapes();
+  let point = new Triangle();
+
+  point.color = [0, 0.8, 0.8, 1.0];
+  point.points = [-2, 1, 2, 1, -2, -2];
+  g_shapesList.push(point);
+  point = new Triangle();
+  point.color = [0, 0.8, 0.8, 1.0];
+  point.points = [-2, -2, 2, 1, 2, -2];
+  g_shapesList.push(point);
+
+
+  point = new Triangle();
+  point.points = [-2, 4, 2, 4, -2, 1];
+  g_shapesList.push(point);
+  point = new Triangle();
+  point.points = [-2, 1, 2, 4, 2, 1];
+  g_shapesList.push(point);
+  point = new Triangle();
+  point.points = [-2, 4, 0, 4.5, 2, 4];
+  g_shapesList.push(point);
+
+  point = new Triangle(); // Ears
+  point.color = [0.3, 0.4, 1.0, 1.0];
+  point.points = [-2, 4, -2.5, 4, -2, 3];
+  g_shapesList.push(point);
+  point = new Triangle();
+  point.color = [0.3, 0.4, 1.0, 1.0];
+  point.points = [2, 4, 3, 3.5, 2, 3];
+  g_shapesList.push(point);
+
+  point = new Triangle();
+  point.color = [0.3, 0.4, 1.0, 1.0];
+  point.points = [-1, -2, -1, -3, -2, -3];
+  g_shapesList.push(point);
+  point = new Triangle();
+  point.color = [0.3, 0.4, 1.0, 1.0];
+  point.points = [1, -2, 1, -3, 2, -3];
+  g_shapesList.push(point);
+
+  point = new Triangle();
+  point.color = [0.3, 0.4, 1.0, 1.0];
+  point.points = [2, -1, 3, -1, 2, -2];
+  g_shapesList.push(point);
+
+  point = new Triangle();
+  point.color = [0, 0.8, 0.8, 1.0];
+  point.points = [-2, -2, 0, -2.5, 2, -2];
+  g_shapesList.push(point);
+
+  point = new Triangle();
+  point.points = [-2, 1, -2.5, 2, -2, 4];
+  g_shapesList.push(point);
+  point = new Triangle();
+  point.points = [2, 4, 2, 1, 2.5, 2.5];
+  g_shapesList.push(point);
+
+  point = new Triangle();
+  point.points = [-2, 0, -2, 1, -3, 1];
+  g_shapesList.push(point);
+  point = new Triangle();
+  point.points = [2, 1, 2, 0, 3, 0];
+  g_shapesList.push(point);
+
+  point = new Triangle();
+  point.points = [-2, 1, -1, 1, -1.5, 0.5];
+  g_shapesList.push(point);
+  point = new Triangle();
+  point.points = [-1, 1, 0, 1, -0.5, 0.5];
+  g_shapesList.push(point);
+  point = new Triangle();
+  point.points = [2, 1, 1, 1, 1.5, 0.5];
+  g_shapesList.push(point);
+  point = new Triangle();
+  point.points = [1, 1, 0, 1, 0.5, 0.5];
+  g_shapesList.push(point);
+
+  point = new Triangle(); // Nose
+  point.color = [0.58, 0.25, 0.0, 1];
+  point.points = [0, 3, -1, 3, -0.5, 2.5];
+  g_shapesList.push(point);
+
+  point = new Triangle(); // Shell
+  point.color = [1.0, 1.0, 0.5, 1];
+  point.points = [0, 0, -1, 0, -1, -1.5];
+  g_shapesList.push(point);
+  point = new Triangle();
+  point.color = [1.0, 1.0, 0.5, 1];
+  point.points = [0, 0, 0, -1.5, -1, -1.5];
+  g_shapesList.push(point);
+  point = new Triangle();
+  point.color = [1.0, 1.0, 0.5, 1];
+  point.points = [0, 0, 0, -1, 0.25, -0.5];
+  g_shapesList.push(point);
+  point = new Triangle();
+  point.color = [1.0, 1.0, 0.5, 1];
+  point.points = [-1, 0, -1.25, -0.5, -1, -1];
+  g_shapesList.push(point); 
+
+  point = new Triangle(); // Right Eye
+  point.color = [0.0, 0.0, 0.0, 1];
+  point.points = [0.75, 3, 1, 3.25, 0.75, 3.5];
+  g_shapesList.push(point);
+  point = new Triangle();
+  point.color = [0.0, 0.0, 0.0, 1];
+  point.points = [0.75, 3, 0.5, 3.25, 0.75, 3.5];
+  g_shapesList.push(point);
+
+  point = new Triangle(); // Left Eye
+  point.color = [0.0, 0.0, 0.0, 1];
+  point.points = [-1.25, 3, -1, 3.25, -1.25, 3.5];
+  g_shapesList.push(point);
+  point = new Triangle();
+  point.color = [0.0, 0.0, 0.0, 1];
+  point.points = [-1.25, 3, -1.5, 3.25, -1.25, 3.5];
+  g_shapesList.push(point);
+
+  point = new Triangle(); // Mouth
+  point.color = [0.0, 0.0, 0.0, 1];
+  point.points = [-0.5, 2, -1, 1.5, 0, 1.5];
+  g_shapesList.push(point);
+  point = new Triangle();
+  point.color = [1.0, 1.0, 1.0, 1];
+  point.points = [-0.5, 1.9, -1, 1.5, 0, 1.5];
+  g_shapesList.push(point);
+
+  point = new Triangle(); // Left Freckles
+  point.color = [0.0, 0.0, 0.0, 1];
+  point.points = [-2, 2.1, -1.9, 2.1, -1.95, 2];
+  g_shapesList.push(point);
+  point = new Triangle();
+  point.color = [0.0, 0.0, 0.0, 1];
+  point.points = [-1.5, 2, -1.4, 2, -1.45, 1.9];
+  g_shapesList.push(point);
+  point = new Triangle();
+  point.color = [0.0, 0.0, 0.0, 1];
+  point.points = [-1.8, 1.8, -1.7, 1.8, -1.75, 1.7];
+  g_shapesList.push(point);
+
+  point = new Triangle(); // Right Freckles
+  point.color = [0.0, 0.0, 0.0, 1];
+  point.points = [1, 1.6, 1.1, 1.6, 1.05, 1.5];
+  g_shapesList.push(point);
+  point = new Triangle();
+  point.color = [0.0, 0.0, 0.0, 1];
+  point.points = [0.9, 1.8, 1.0, 1.8, 0.95, 1.7];
+  g_shapesList.push(point);
+  point = new Triangle();
+  point.color = [0.0, 0.0, 0.0, 1];
+  point.points = [1.2, 1.8, 1.3, 1.8, 1.25, 1.7];
+  g_shapesList.push(point);
+
+  renderAllShapes();
+
+}
+
+function main() {
+
+  setupWebGL();
+  connectVariablesToGLSL();
+
+  addActionsForHTMLUI();
+
+  // Register function (event handler) to be called on a mouse press
+  canvas.onmousedown = click;
+  canvas.onmousemove = function(ev) { if(ev.buttons == 1) { click(ev) }};
+
+  // Specify the color for clearing <canvas>
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+  // Clear <canvas>
+  gl.clear(gl.COLOR_BUFFER_BIT);
+  renderAllShapes();
+}
+
+var g_shapesList = [];
+
+var g_points = [];  // The array for the position of a mouse press
+var g_colors = [];  // The array to store the color of a point
+var g_sizes = []; // The array to store the size of a point
+
+function click(ev) {
+  [x, y] = convertCoordinatesEventToGL(ev);
+  let point;
+  if (g_selectedType==POINT) {
+    point = new Point();
+  } else if (g_selectedType==TRIANGLE) {
+    point = new Triangle();
+  } else {
+    point = new Circle();
+    point.segments = g_selectedSegment;
+  }
+
+  point.position = [x, y];
+  point.color = g_selectedColor.slice();
+  point.size = g_selectedSize;
+  g_shapesList.push(point);
+  undone_Shapes = [];
+  /*// Store the coordinates to g_points array
+  g_points.push([x, y]);
+  // Store the colors to g_colors array
+  g_colors.push(g_selectedColor.slice());
+  g_sizes.push(g_selectedSize);*/
+
+  renderAllShapes();
+}
+
+function convertCoordinatesEventToGL(ev) {
+  var x = ev.clientX; // x coordinate of a mouse pointer
+  var y = ev.clientY; // y coordinate of a mouse pointer
+  var rect = ev.target.getBoundingClientRect();
+
+  x = ((x - rect.left) - canvas.width/2)/(canvas.width/2);
+  y = (canvas.height/2 - (y - rect.top))/(canvas.height/2);
+
+  return([x, y]);
+}
+
+function renderAllShapes() {
+  // Check the time at the start of this function
+  var startTime = performance.now();
+
+  var globalRotMat = new Matrix4().rotate(g_globalAngle, 0, 1, 0);
+  gl.uniformMatrix4fv(u_GlobalRotateMatrix, false, globalRotMat.elements);
+
+  // Clear <canvas>
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+  //var len = g_shapesList.length;
+  //for(var i = 0; i < len; i++) {
+  //  g_shapesList[i].render();
+  //}
+
+  var rightShoulder = new Cube();
+  rightShoulder.color = [0.5, 0.8, 1.0, 1.0];
+  rightShoulder.matrix.scale(0.65, 0.25, 0.25);
+  rightShoulder.matrix.translate(-1.0, 0, 0);
+  rightShoulder.render();
+
+  var rightArm = new Cube();
+  rightArm.color = [0.2, 0.1, 0.1, 1.0];
+  rightArm.matrix.scale(0.3, 0.25, 0.25);
+  rightArm.matrix.translate(0.0, 0, 0);
+  rightArm.render();
+
+  var rightHand = new Cube();
+  rightHand.color = [0.2, 0.1, 0.1, 1.0];
+  rightHand.matrix.rotate(45, 0, 0);
+  rightHand.matrix.scale(0.2, 0.1, 0.25);
+  rightHand.matrix.translate(1.5, -1.0, 0);
+  rightHand.render();
+
+  var duration = performance.now() - startTime;
+  sendTextToHTML("numdot: " + 0 + "ms: " + Math.floor(duration) + "fps: " + Math.floor(10000/duration)/10, "numdot");
+}
+
+function sendTextToHTML(text, htmlID) {
+  var htmlElm = document.getElementById(htmlID);
+  if (!htmlElm) {
+    console.log("Failed to get " + htmlID + " from HTML");
+    return;
+  }
+  htmlElm.innerHTML = text;
+}
